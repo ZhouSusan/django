8a9e489b39ada68a16167d0c3a8ef69ca90ac8ca
ZhouSusan <szhou089@gmail.com>
2022-08-31 22:32:23 -0700

    "added paths to + fixed syntax error  in polls/views.py"

--- polls/views.py
+++ polls/views.py
@@ -6,10 +6,10 @@ def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 def detail(request, question_id):
-    return HttpResponse("You are looking at Question %s." %question_id)
+    return HttpResponse("You are looking at Question %s." % question_id)
 
 def results(resquest, question_id):
-    return HttpResponse("You are looking at the results of Question %s". %question_id)
+    return HttpResponse("You are looking at the results of Question %s." % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on Question %s". %question_id)
+    return HttpResponse("You're voting on Question %s." % question_id)
