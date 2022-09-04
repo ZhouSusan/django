806d1902db42dcc6100619a497cbf816a477c600
ZhouSusan <szhou089@gmail.com>
2022-09-03 23:59:23 -0700

    "updated index in polls/views.py"

--- polls/views.py
+++ polls/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    last_question_list = Question.objects.order_by('pub_date')[:5]
+    output = ','.join([q.question_text for q in last_question_list])
+    return HttpResponse(output)
 
 def detail(request, question_id):
     return HttpResponse("You are looking at Question %s." % question_id)
